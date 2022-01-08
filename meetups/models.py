9b67126242abd8828ebf3dde15dc95852e8401c0
Andres Hernandez <edanhebla1213@hotmail.com>
2022-01-08 00:17:35 -0500

    side note relationships models

--- meetups/models.py
+++ meetups/models.py
@@ -22,8 +22,8 @@ class Meetup(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    partipants = models.ManyToManyField(Participant, blank=True, null=True)
-
+    location = models.ForeignKey(Location, on_delete=models.CASCADE) # one to many relation 
+    partipants = models.ManyToManyField(Participant, blank=True, null=True) # many to many relation
+    # models.OneToOneField() for one to one relation
     def __str__(self):
         return f'{self.title} - {self.slug}'
