bab04afddf3574a85dc9dda71120ac03f549042e
Andres Hernandez <edanhebla1213@hotmail.com>
2022-01-08 10:23:06 -0500

    side note forms

--- meetups/forms.py
+++ meetups/forms.py
@@ -6,6 +6,11 @@ can also build a form object which give us all the validation logic and so on wi
 define all the form fields on our own'''
 class RegistrationsForm(forms.Form):
     email = forms.EmailField(label='Your email')
+    '''
+    Use a modelform is kind of redundant here because we are not using the save method anymore
+    model form is forms.modelForm this is convenient if we want to infere a form from a model and use the
+    save method. But we can also build a form object with all the validation login and so on with forms.Form
+    '''
     # class Meta:
     #     model = Participant
     #     fields = ['email']
